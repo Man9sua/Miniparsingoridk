2766bbee4a7af3926645758679094fe7e27d305f
Man9sua <161411954+Man9sua@users.noreply.github.com>
2025-05-25 19:24:39 +0500

    Update checkpagedocs.py

--- checkpagedocs.py
+++ checkpagedocs.py
@@ -6,26 +6,26 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-# URL для логина
+# URL
 login_url = 'https://eduway.kz/login'
-username = "Mansua"
-password = "Mansur09"  # Используйте безопасный способ хранения пароля, например, переменные окружения
+username = "urnickname"
+password = "ifitneeds"  
 
-# Настраиваем сессию для работы с requests
+
 session = requests.Session()
 
-# Функция для получения CSRF токена
+
 def get_login_token(session):
     response = session.get(login_url)
     soup = BeautifulSoup(response.text, 'html.parser')
     
-    # Ищем скрытое поле для CSRF-токена
+    
     csrf_token = soup.find('input', {'name': '_csrf'})
     
-    # Если находим токен, возвращаем его значение, иначе None
+    
     return csrf_token['value'] if csrf_token else None
 
-# Попытка входа в систему
+
 def login_with_requests(session, username, password):
     csrf_token = get_login_token(session)
     if not csrf_token:
@@ -46,28 +46,28 @@ def login_with_requests(session, username, password):
         print("Login failed")
         return False
 
-# Пытаемся выполнить вход
+
 if login_with_requests(session, username, password):
-    # Настраиваем Selenium для перехода на страницу после логина
+   
     driver_path = 'drivers/chromedriver.exe'
     service = Service(driver_path)
     driver = webdriver.Chrome(service = service)
 
     try:
-        # Переносим cookies из requests в браузер
-        driver.get("https://eduway.kz/student")  # Инициализируем сессию в браузере
+     
+        driver.get("https://eduway.kz/student")  
         for cookie in session.cookies:
             driver.add_cookie({'name': cookie.name, 'value': cookie.value})
 
-        # Переходим на страницу с результатами экзаменов
+       
         driver.get("https://eduway.kz/student/exam-results")
-        time.sleep(2)  # Даем время на загрузку страницы
+        time.sleep(2)  
 
-        # Переходим на страницу с конкретными результатами экзамена
+        
         driver.get("https://eduway.kz/student/exam-results/exam/29")
         time.sleep(2)
 
-        # Получаем содержимое страницы
+       
         page_content = driver.find_element(By.TAG_NAME, 'body').text
         print("Содержимое страницы:")
         print(page_content)
